88894d77a6bbe67036cd4e0dc5a11da028dc25da
Ashok Soni <ashok.soni@gmail.com>
2016-12-21 16:35:52 +0530

    So Jack memory issue won't occur.
    Change-Id: I823d588b52e3a04acac7296b90600a8522e6aa42

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,6 @@
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+./prebuilts/sdk/tools/jack-admin kill-server
+./prebuilts/sdk/tools/jack-admin start-server
 . vendor/cm/tools/rr_variant.sh && ./vendor/cm/tools/changelog.sh &&
 function hmm() {
 cat <<EOF
